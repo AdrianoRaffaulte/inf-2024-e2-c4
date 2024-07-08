82f05f0d70f4ad1b2836d495dcca89e16c167979
AdrianoRaffaulte <raffaulteadrianog@gmail.com>
2024-07-08 16:39:40 -0300

    Agregamos nuevo problema

---
+++ problema2.py
@@ -0,0 +1,10 @@
+# Obtencion de edades
+hermano1 = 10
+hermano2 = 10
+
+if hermano1 > hermano2:
+    print("El hermano1 es mayor que el hermano2")
+elif hermano2 > hermano1:
+    print("El hermano2 es mayor que el hermano1")
+else:
+    print("Los hermanos tienen la misma edad")
